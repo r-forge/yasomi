d9602df70cdb179ea285484b58538d98d9eb4c03
frossi (Prof. ) <Fabrice.Rossi@apiacoa.org>
2011-03-04 16:13:51 +0000

    Added missing declaration (second_bmu).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yasomi@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yasomi/src/bmu.h
+++ pkg/yasomi/src/bmu.h
@@ -4,6 +4,9 @@
 int bmu(double *proto,int *nproto,double *data,int *ndata,int *dim,
 	double *weights,int *winner,double *error);
 
+void second_bmu(double *proto,int *nproto,double *data,int *ndata,int *dim,
+		int *winners);
+
 int bmu_heskes(double *proto,double *neigh,int *nproto,double *data,
 	       int *ndata,int *dim,double *weights,int *winner,double *error);
 
